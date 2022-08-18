c7e741b55133fb753c000c78600c3f6f0b7068f2
dchaitu <chaitanyadokara@gmail.com>
2022-08-18 20:32:18 +0530

    methods written to post

--- fb_post/models.py
+++ fb_post/models.py
@@ -5,20 +5,22 @@ from django.db import models
 class User(models.Model):
     user_id = models.CharField(max_length=10,primary_key=True)
     name = models.CharField(max_length=100)
-    profile_pic= models.TextField()
+    profile_pic = models.TextField()
 
 
 class Post(models.Model):
+    post_id = models.CharField(max_length=10, primary_key=True)
     content = models.CharField(max_length=1000)
     posted_at = models.DateTimeField()
     posted_by = models.ForeignKey(User,on_delete=models.CASCADE)
 
 class Comment(models.Model):
+    comment_id = models.CharField(max_length=10, primary_key=True)
     content = models.CharField(max_length=1000)
     commented_at = models.DateTimeField()
     commented_by = models.ForeignKey(User,on_delete=models.CASCADE)
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
-    # reply = models.ForeignKey(Comment)
+    reply = models.ForeignKey('self',on_delete=models.CASCADE)
 
 class React(models.Model):
     CHOICES=(
--- fb_post/utils/tasks.py
+++ fb_post/utils/tasks.py
@@ -13,7 +13,7 @@ def create_post(user_id, post_content):
 
     post = Post.objects.create(posted_by=user,content=post_content,posted_at=datetime.now())
 
-    return post.id
+    return post.post_id
 
 
 def create_comment(user_id, post_id, comment_content):
@@ -29,4 +29,19 @@ def create_comment(user_id, post_id, comment_content):
 
     comment.save()
 
-    return comment
+    return comment.comment_id
+
+def reply_to_comment(user_id, comment_id, reply_content):
+    """
+    :returns: comment_id
+    """
+    user = User.objects.get(user_id=user_id)
+    reply = Comment.objects.create(commented_by=user,content=reply_content,comment_id=comment_id)
+    reply.save()
+
+def react_to_post(user_id, post_id, reaction_type):
+    """
+    """
+
+    post = Post.objects.get(post_id= post_id)
+    react = React.objects.create(reacted_at=datetime.now(),reacted_by=user_id,post=post,react =reaction_type)
